7fc9cc9f1109ea10e880d5a34fc65a4bc414266d
gleffelman <gtl19102@uga.edu>
2017-04-16 17:46:56 -0400

    Create SourceDeleteQuery.java

---
+++ SourceDeleteQuery.java
@@ -0,0 +1,57 @@
+/**
+ * 
+ */
+package dbHelpers;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+/**
+ * @author craigpiercy
+ *
+ */
+public class SourceDeleteQuery {
+	
+	private Connection connection;
+	
+	public SourceDeleteQuery(String dbName, String uname, String pwd){
+		
+		String url = "jdbc:mysql://localhost:3306/"+dbName;
+		
+		try {
+			Class.forName("com.mysql.jdbc.Driver").newInstance();
+			this.connection = DriverManager.getConnection(url, uname, pwd);
+		} catch (InstantiationException | IllegalAccessException
+				| ClassNotFoundException | SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public void doDelete(int sourceID){
+		// set up a String to hold our query
+		String query = "delete from sources where sourceID = ?";
+		
+		// create a preparedstatement using our query string
+		try {
+			PreparedStatement ps = connection.prepareStatement(query);
+			
+			// fill in the preparedstatement
+			ps.setInt(1, sourceID);
+			
+			// execute the query
+			ps.executeUpdate();
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+		
+		
+	}
+
+}
