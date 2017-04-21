906c939537a081bda2e9a949c246ea627a291072
KBailey95 <kirk.bailey25@uga.edu>
2017-04-21 16:25:55 -0400

    Add files via upload

--- UserAddQuery.java
+++ UserAddQuery.java
@@ -1,12 +1,45 @@
-/**
- * 
- */
 package dbHelpers;
 
-/**
- * @author Kirk
- *
- */
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import model.User;
+
 public class UserAddQuery {
+	
+public Connection connection;
+	
+	public UserAddQuery(String dbName, String uname, String pwd){
+		String url = "jdbc:mysql://localhost:3306/" + dbName;
+		
+		try {
+			Class.forName("com.mysql.jdbc.Driver").newInstance();
+			this.connection = DriverManager.getConnection(url, uname, pwd);
+		} catch (InstantiationException | IllegalAccessException | ClassNotFoundException | SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public void doAdd(User user){
+		String query = "insert into fakenwews(email, password) values(?,?,?,?,?,?)";
+		
+		
+		try {
+			PreparedStatement ps = connection.prepareStatement(query);
+			
+			ps.setString(1,  user.getEmail());
+			ps.setString(2, user.getUpwd());
+			
+			
+			ps.executeUpdate();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
 
 }
