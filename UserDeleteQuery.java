d9b929bf907d62c8d3e80be15fab2684f4560f2e
KBailey95 <kirk.bailey25@uga.edu>
2017-04-21 16:27:40 -0400

    Add files via upload

--- UserDeleteQuery.java
+++ UserDeleteQuery.java
@@ -3,10 +3,54 @@
  */
 package dbHelpers;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import model.User;
+
 /**
  * @author Kirk
  *
  */
 public class UserDeleteQuery {
+	
+private Connection connection;
+	
+	public UserDeleteQuery(String dbName, String uname, String pwd){
+		
+	String url = "jdbc:mysql://localhost:3306/"+dbName;
+	
+	try {
+		Class.forName("com.mysql.jdbc.Driver").newInstance();
+		this.connection = DriverManager.getConnection(url, uname, pwd);
+	} catch (InstantiationException | IllegalAccessException | ClassNotFoundException | SQLException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+	
+	}
+	
+	public void doDelete(String email){
+		//set up a string to hold our query
+		String query = "delete from user where email = ?";
+		
+		//create e preparedstatement using our query string
+		try {
+			PreparedStatement ps = connection.prepareStatement(query);
+			
+			//fill in the preparedstatement
+			ps.setString(1, email);
+			
+			//execute the query
+			ps.executeUpdate();
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 
 }
+
--- UserReadServlet.java
+++ UserReadServlet.java
@@ -1,16 +1,22 @@
 package controllers;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+//import dbHelpers.ReadQuery;
+import dbHelpers.UserReadQuery;
+import model.User;
+
 /**
- * Servlet implementation class UserReadServlet
+ * Servlet implementation class ReadUserServlet
  */
-@WebServlet("/UserReadServlet")
+@WebServlet("/read")
 public class UserReadServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
@@ -18,7 +24,7 @@ public class UserReadServlet extends HttpServlet {
      * @see HttpServlet#HttpServlet()
      */
     public UserReadServlet() {
-        super();
+    	super();
         // TODO Auto-generated constructor stub
     }
 
@@ -26,16 +32,27 @@ public class UserReadServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		doPost(request, response);
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		// Create a ReadQuery helper object
+		UserReadQuery ru = new UserReadQuery("fakenews", "root", "root");
+		//Get the html table from the ReadQuery object
+		ru.doRead();
+		String table = ru.getHTMLTable();
+		//pass execution control to read.jsp along with the table
+		request.setAttribute("table", table);
+		String url = "/read.jsp";
+		
+		RequestDispatcher dispatcher = request.getRequestDispatcher(url);
+		dispatcher.forward(request, response);
+		
 	}
 
 }
+
